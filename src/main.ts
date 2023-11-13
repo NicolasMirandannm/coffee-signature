b2af3aad095394e2c1f841ef184eabc36f491896
Eliel Aguiar <eliel2013aguiar@hotmail.com>
2023-11-13 19:54:13 -0400

    configuração de swagger na aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger')
+    .setDescription(
+      'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, nessa aplicação utilizaremos para descrever cada endpoint e estrutura de entidades presentes nele.',
+    )
+    .setVersion('3.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
